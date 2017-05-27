40b70afb29d09f4ed604b04873a4589f96423b06
s3strm <s3strm@fastmail.com>
2017-05-27 10:48:53 +1000

    Actually do some remuxing

--- bootstrap/run.sh
+++ bootstrap/run.sh
@@ -3,11 +3,12 @@ AZ=$(curl 169.254.169.254/latest/meta-data/placement/availability-zone/)
 export AWS_DEFAULT_REGION=${AZ::-1}
 export INSTANCE_ID
 
-export MOVIES_BUCKET=$(
+MOVIES_BUCKET=$(
   aws cloudformation list-exports \
     --query 'Exports[?Name==`s3strm-movies-bucket`].Value' \
     --output text
 )
+export MOVIES_BUCKET
 
 AUTO_SCALING_GROUP=$(
   aws cloudformation describe-stacks \
@@ -16,27 +17,42 @@ AUTO_SCALING_GROUP=$(
     --output text
 )
 
-function get_mp4() {
-  aws s3 ls s3://${MOVIES_BUCKET}/incoming/ \
+function find_video() {
+  aws s3 ls "s3://${MOVIES_BUCKET}/incoming/" \
     | grep -m1 \.mp4$ \
-    | grep -E -o [^\ ]+$
+    | grep -E -o "[^\ ]+$"
   return $?
 }
 
+function download() {
+  aws s3 cp "s3://${MOVIES_BUCKET}/incoming/$1" "/tmp/$1"
+}
+
+function upload() {
+  aws s3 cp "/tmp/${video}" "s3://${MOVIES_BUCKET}/${imdb_id}/video.mp4" \
+    && aws s3 rm "s3://${MOVIES_BUCKET}/incoming/${video}"
+}
+
+function cleanup() {
+  echo "Scaling down because there are no more videos to process"
+  aws autoscaling update-auto-scaling-group \
+    --auto-scaling-group-name "${AUTO_SCALING_GROUP}" \
+    --desired-capacity 0
+}
+
+trap '{ cleanup; }' EXIT
+
 while true; do
-  video="$(get_mp4)"
-  if [[ ! -z ${video} ]]; then
-    imdb_id="$(basename ${video} .mp4)"
-    aws s3 cp s3://${MOVIES_BUCKET}/incoming/${video} /tmp/${video}
-    aws s3 cp /tmp/${video} s3://${MOVIES_BUCKET}/${imdb_id}/video.mp4 \
-      && aws s3 rm s3://${MOVIES_BUCKET}/incoming/${video} \
-      && rm /tmp/${video}
-  else
-    echo "Scaling down because there are no more videos to process"
-    aws autoscaling update-auto-scaling-group \
-      --auto-scaling-group-name ${AUTO_SCALING_GROUP} \
-      --desired-capacity 0
-    exit 0
-  fi
+  video="$(find_video)"
+  [[ -z ${video} ]] && exit 0
+
+  video_extension=$(echo "${video}" | grep -E -o "\.[^\.]+$")
+  imdb_id="$(basename "${video}" "${video_extension}")"
+  download "${video}"
+  remux "${video}"
+  video="${imdb_id}.mp4"
+  upload "${video}"
+  find /tmp -iname "${imdb_id}.*" -delete
+
   unset video imdb_id
 done
